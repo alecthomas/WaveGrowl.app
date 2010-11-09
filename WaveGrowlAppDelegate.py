7a2d7185322221001d80e0039a97a5f128198c59
Alec Thomas <alec@swapoff.org>
2010-11-09 12:28:20 +1100

    Update license.

--- WaveGrowlAppDelegate.py
+++ WaveGrowlAppDelegate.py
@@ -1,10 +1,18 @@
+# encoding: utf-8
 #
-#  WaveGrowlAppDelegate.py
-#  WaveGrowl
+# Copyright 2010 Alec Thomas <alec@swapoff.org>
 #
-#  Created by Alec Thomas on 22/07/10.
-#  Copyright __MyCompanyName__ 2010. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 from AppKit import NSWorkspace, NSStatusBar, NSMenuItem, NSMenu, NSImage, \
